6f840036e140a4b2cf0a26357c1b6849e8b351a1
derekdesrosiers <derekdvd111@gmail.com>
2024-04-09 12:27:27 -0400

    Update 4/9/24

--- app/src/main/java/com/example/appsforgood/MainActivity.java
+++ app/src/main/java/com/example/appsforgood/MainActivity.java
@@ -8,17 +8,27 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import android.app.Activity;
+import android.telephony.SmsManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
+
+
 }
