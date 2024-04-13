fa5833352fe9a21aa74913d61526029ea858e2a3
derekdesrosiers <derekdvd111@gmail.com>
2024-04-12 21:46:16 -0400

    Update 4/12/24

--- app/src/main/java/com/example/appsforgood/BusSchedules.java
+++ app/src/main/java/com/example/appsforgood/BusSchedules.java
@@ -1,5 +1,6 @@
 package com.example.appsforgood;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,17 +9,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class BusSchedules extends AppCompatActivity {
+import android.view.View;
+import android.widget.Button;
+import android.widget.Button;
 
+public class BusSchedules extends AppCompatActivity {
+    Button backButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_bus_schedules);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        backButton=findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
         });
     }
 }
