9eca0d4173b09afc07b65e8f1886a6097aff61f2
derekdesrosiers <derekdvd111@gmail.com>
2024-04-12 21:46:16 -0400

    Update 4/12/24

--- app/src/main/java/com/example/appsforgood/UpdatesFeedback.java
+++ app/src/main/java/com/example/appsforgood/UpdatesFeedback.java
@@ -8,17 +8,33 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class UpdatesFeedback extends AppCompatActivity {
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+
+public class UpdatesFeedback extends AppCompatActivity {
+Button backButton;
+EditText inputText;
+TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_updates_feedback);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        backButton=findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
         });
+        textView = (TextView) findViewById(R.id.textView);
+        inputText = (EditText) findViewById(R.id.inputText);
+
+    }
+    public void updateText(View view) {
+        textView.setText("Thank you!");
+        System.out.println("Button pressed");
     }
 }
