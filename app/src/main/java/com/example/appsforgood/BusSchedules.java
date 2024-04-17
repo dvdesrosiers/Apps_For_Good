20b70c5b683f0e8f1fad6a186ce0ef277498804e
derekdesrosiers <derekdvd111@gmail.com>
2024-04-17 13:37:02 -0400

    Google Maps WebView update; debugged bus schedules page

--- app/src/main/java/com/example/appsforgood/BusSchedules.java
+++ app/src/main/java/com/example/appsforgood/BusSchedules.java
@@ -1,57 +1,25 @@
 package com.example.appsforgood;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.Toast;
-import android.widget.Spinner;
-
-import java.util.Arrays;
+import android.widget.Button;
 
 public class BusSchedules extends AppCompatActivity {
     Button backButton;
-
-    String[] item = {"Route1", "Route2", "Route3", "Route4", "Route5", "Route6", "Route7",
-            "Route 8", "Route9", "Route10", "Route 11", "Route 12", "Route 13", "Route 14",
-            "Route 15", "Route 16", "Route 17", "Route 18", "Route 19", "Route 20", "Route 21",
-            "Route 22", "Route 23", "Route 24", "Route 25", "Route 26", "Route 27","Route 28",
-            "Route 29","Route 30", "Route 31"};
-    AutoCompleteTextView autoCompleteTextView;
-
-
-    ArrayAdapter<String> adapterItems;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bus_schedules);
-
-
-        autoCompleteTextView = findViewById(R.id.auto_complete_txt);
-
-        adapterItems = new ArrayAdapter<>(this, R.layout.list_item, item);
-
-        autoCompleteTextView.setAdapter(adapterItems);
-
-        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                        String item2 = parent.getItemAtPosition(position).toString();
-
-                Toast.makeText(getApplicationContext(),"Item: " + item2,Toast.LENGTH_SHORT).show();
-
-            }
-        });
-
-
         backButton=findViewById(R.id.backButton);
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,4 +29,4 @@ public class BusSchedules extends AppCompatActivity {
         });
     }
 
-}
+   }
---
+++ app/src/main/java/com/example/appsforgood/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.appsforgood;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import android.app.Activity;
+import android.telephony.SmsManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.widget.Toast;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    Button button;
+    Button button2;
+    Button button3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button=(Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, UpdatesFeedback.class);
+                startActivity(intent);
+            }
+        });
+        button2=(Button)findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,BusSchedules.class);
+                startActivity(intent);
+            }
+        });
+        button3=(Button)findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,test.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+    }
+
+
+}
