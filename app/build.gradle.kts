fee0880db7c74c95766a89810508ac875968d538
derekdesrosiers <derekdvd111@gmail.com>
2024-04-30 09:38:00 -0400

    SMS send

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,8 @@ dependencies {
     implementation(libs.ui.graphics)
     implementation(libs.ui.tooling.preview)
     implementation(libs.material3)
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
