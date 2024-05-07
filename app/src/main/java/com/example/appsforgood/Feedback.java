4a10e235be7164dc604b26a9c40a128910e32155
derekdesrosiers <derekdvd111@gmail.com>
2024-05-06 21:27:00 -0400

    Feedback page update

--- app/src/main/java/com/example/appsforgood/Feedback.java
+++ app/src/main/java/com/example/appsforgood/Feedback.java
@@ -1,26 +1,37 @@
 package com.example.appsforgood;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.appsforgood.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class Feedback extends AppCompatActivity {
     Button backButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_feedback);
+
         backButton=findViewById(R.id.backButton);
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,12 +41,33 @@ public class Feedback extends AppCompatActivity {
                 finish();
             }
         });
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        TextInputEditText date = findViewById(R.id.dateET);
+        TextInputEditText issue = findViewById(R.id.issueET);
+        MaterialButton addAlert = findViewById(R.id.addAlert);
+
+        addAlert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Map<String, Object> alert = new HashMap<>();
+                alert.put("date", Objects.requireNonNull(date.getText()).toString());
+                alert.put("issue", Objects.requireNonNull(issue.getText()).toString());
+
+                db.collection("feedback").add(alert).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Toast.makeText(Feedback.this, "Feedback sent successfully", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(Feedback.this, "Failed to send feedback", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
         });
-
-
     }
 }
