c838fe59c65cc110d9103001ca170d4fa30af632
yerinsk <164551102+yerinsk@users.noreply.github.com>
2024-05-14 15:54:50 -0400

    Update App.java
    commented

--- app/src/main/java/com/example/appsforgood/App.java
+++ app/src/main/java/com/example/appsforgood/App.java
@@ -1,7 +1,10 @@
 package com.example.appsforgood;
 
+// Import statements for necessary Android libraries and components
 import android.app.Application;
 
+// Custom application class
 public class App extends Application {
+    // Static variable to hold an instance of an alert
     public static Alerts alert;
 }
